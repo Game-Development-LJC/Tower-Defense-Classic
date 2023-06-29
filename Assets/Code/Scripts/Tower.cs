1fa76b882f521d84b82be6d6298b8357871f993d
PS218820 <PS218820@summacollege.nl>
2023-06-29 14:25:20 +0200

    Torens schieten projectielen af & Projectielen komen van een toren af & Projectiel gaan op een vijand af

--- Assets/Code/Scripts/Tower.cs
+++ Assets/Code/Scripts/Tower.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject projectilePrefab;
+    public Transform targetObject;
+    
+    public float fireRate = 1f;
+    
+    private float nextFireTime;
+
+    public void ShootProjectile()
     {
-        
+        if (targetObject != null)
+        {
+            // Calculate direction towards the target object
+            Vector2 direction = (targetObject.position - transform.position).normalized;
+
+            // Instantiate the projectile
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+            // Set the direction for the projectile to move towards the target object
+            Projectile projectileComponent = projectile.GetComponent<Projectile>();
+            if (projectileComponent != null)
+            {
+                projectileComponent.SetDirection(direction);
+            }
+        }
     }
 
-    // Update is called once per frame
+    // Example usage: Call this function from another script or event to trigger the tower to shoot
     void Update()
     {
-        
+        // Check if enough time has passed to fire another projectile
+        if (Time.time >= nextFireTime)
+        {
+            // Fire projectile and set the next allowed fire time
+            ShootProjectile();
+            nextFireTime = Time.time + 1f / fireRate;
+        }
     }
 }
